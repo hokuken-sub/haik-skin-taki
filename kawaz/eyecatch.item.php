f64fe1926600bd8e682007e4b9cbf0ec4f9295ab
hokuken <customer@hokuken.com>
2013-09-26 20:09:35 +0900

    紛れ込んでいた他デザインを削除

--- kawaz/eyecatch.item.php
+++
@@ -1,7 +0,0 @@
-<div class="item#{$active}">
-	#{$image}
-	<div class="carousel-caption">
-		<h1 style="#{$title_color}#{$title_size}">#{$title}</h1>
-		<div class="lead" style="#{$content_color}#{$content_size}">#{$content}</div>
-	</div>
-</div>
--- semi/eyecatch.wrap.php
+++
@@ -1,7 +0,0 @@
-<div class="eyecatch carousel slide #{$single} #{$fit}">
-	<div class="carousel-inner">
-		#{$items}
-	</div>
-	<a href="#orgm_eyecatch .eyecatch" class="carousel-control left" data-slide="prev">&lsaquo;</a>
-	<a href="#orgm_eyecatch .eyecatch" class="carousel-control right" data-slide="next">&rsaquo;</a>
-</div>
