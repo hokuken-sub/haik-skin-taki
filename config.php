85872e960d8847b8dcb4df6fd9750014b4808cb2
hokuken <customer@hokuken.com>
2014-01-07 12:52:49 +0900

    v1.1.0

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array(
 	'name' => 'taki',
-	'version' => '1.0.2',
+	'version' => '1.1.0',
 	'thumbnail' => 'img/thumbnail.png',
 	'style_file' => 'css/docs.css',
 	'templates' => array(
